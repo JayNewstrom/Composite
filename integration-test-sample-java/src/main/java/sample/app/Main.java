50c2b8e5b3bf73adda47d85dd50469f986c0c7fc
Jay Newstrom <jaynewstrom@gmail.com>
2017-06-14 17:12:40 -0500

    Exercise reflection library to ensure it works!

--- integration-test-sample-java/src/main/java/sample/app/Main.java
+++ integration-test-sample-java/src/main/java/sample/app/Main.java
@@ -1,11 +1,19 @@
 package sample.app;
 
+import com.jaynewstrom.composite.reflection.CompositeReflection;
+
+import java.util.Set;
+
 import sample.library.NavigationRegistrable;
 
 public final class Main {
     public static void main(String[] args) {
         for (NavigationRegistrable navigationRegistrable : GeneratedNavigationRegistrableModule.modules()) {
-            System.out.println(navigationRegistrable.getClass().getName());
+            System.out.println("Compile Time! " + navigationRegistrable.getClass().getName());
+        }
+        Set<NavigationRegistrable> registrableSet = CompositeReflection.modules(NavigationRegistrable.class);
+        for (NavigationRegistrable navigationRegistrable : registrableSet) {
+            System.out.println("Reflection! " + navigationRegistrable.getClass().getName());
         }
     }
 }
--- reflection/src/main/java/com/jaynewstrom/composite/reflection/CompositeReflection.java
+++ reflection/src/main/java/com/jaynewstrom/composite/reflection/CompositeReflection.java
@@ -11,46 +11,51 @@ import java.util.Set;
 public final class CompositeReflection {
     private static final Reflections REFLECTIONS = new Reflections("com.jaynewstrom.composite.generated");
 
-    @SuppressWarnings("unchecked")
     public static <M> Set<M> modules(Class<M> moduleClass) {
         String moduleClassName = moduleClass.getName();
         Set<M> modules = new LinkedHashSet<M>();
         Set<Class<?>> libraryModuleIndexers = REFLECTIONS.getTypesAnnotatedWith(LibraryModuleIndexer.class);
         for (Class<?> libraryModuleIndexerClass : libraryModuleIndexers) {
             for (Annotation annotation : libraryModuleIndexerClass.getDeclaredAnnotations()) {
-                if (annotation instanceof LibraryModuleIndexer) {
-                    LibraryModuleIndexer libraryModuleIndexer = (LibraryModuleIndexer) annotation;
-                    if (libraryModuleIndexer.value().equals(moduleClassName)) {
-                        try {
-                            modules.add((M) Class.forName(libraryModuleIndexer.libraryModule()));
-                        } catch (ClassNotFoundException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
+                M module = moduleFromAnnotation(annotation, moduleClassName);
+                if (module != null) {
+                    modules.add(module);
                 }
             }
         }
         return modules;
     }
 
-    @SuppressWarnings("unchecked")
     public static <M> M module(Class<M> moduleClass) {
         String moduleClassName = moduleClass.getName();
         Set<Class<?>> libraryModuleIndexers = REFLECTIONS.getTypesAnnotatedWith(LibraryModuleIndexer.class);
         for (Class<?> libraryModuleIndexerClass : libraryModuleIndexers) {
             for (Annotation annotation : libraryModuleIndexerClass.getDeclaredAnnotations()) {
-                if (annotation instanceof LibraryModuleIndexer) {
-                    LibraryModuleIndexer libraryModuleIndexer = (LibraryModuleIndexer) annotation;
-                    if (libraryModuleIndexer.value().equals(moduleClassName)) {
-                        try {
-                            return (M) Class.forName(libraryModuleIndexer.libraryModule());
-                        } catch (ClassNotFoundException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
+                M module = moduleFromAnnotation(annotation, moduleClassName);
+                if (module != null) {
+                    return module;
                 }
             }
         }
         throw new IllegalStateException("Module not found.");
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <M> M moduleFromAnnotation(Annotation annotation, String moduleClassName) {
+        if (annotation instanceof LibraryModuleIndexer) {
+            LibraryModuleIndexer libraryModuleIndexer = (LibraryModuleIndexer) annotation;
+            if (libraryModuleIndexer.value().equals(moduleClassName)) {
+                try {
+                    return (M) Class.forName(libraryModuleIndexer.libraryModule()).newInstance();
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                } catch (InstantiationException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return null;
     }
 }
