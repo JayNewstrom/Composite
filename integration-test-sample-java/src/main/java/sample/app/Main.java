69c6d41b831473c306ead138bc3547bc4d054e74
Jay Newstrom <jaynewstrom@gmail.com>
2017-06-14 10:04:56 -0500

    Static module(s) methods, and private constructors for generated app modules.

--- integration-test-sample-java/src/main/java/sample/app/Main.java
+++ integration-test-sample-java/src/main/java/sample/app/Main.java
@@ -4,7 +4,7 @@ import sample.library.NavigationRegistrable;
 
 public final class Main {
     public static void main(String[] args) {
-        for (NavigationRegistrable navigationRegistrable : new GeneratedNavigationRegistrableModule().modules()) {
+        for (NavigationRegistrable navigationRegistrable : GeneratedNavigationRegistrableModule.modules()) {
             System.out.println(navigationRegistrable.getClass().getName());
         }
     }
